b0f59c5a7ddb9195ad229a3c2c85656122bcc902
afpro <admin@afpro.net>
2018-08-31 02:05:07 +0800

    init

---
+++ src/main/kotlin/net/afpro/idea/lancet/Global.kt
@@ -0,0 +1,8 @@
+package net.afpro.idea.lancet
+
+import com.intellij.openapi.diagnostic.Logger
+
+
+object Global {
+    val logger = Logger.getInstance("Lancet")
+}
---
+++ src/main/kotlin/net/afpro/idea/lancet/JavaAnnotator.kt
@@ -0,0 +1,11 @@
+package net.afpro.idea.lancet
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.psi.PsiElement
+
+
+class JavaAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+    }
+}
