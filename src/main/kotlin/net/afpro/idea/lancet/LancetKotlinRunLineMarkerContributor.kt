9a921c7dc06192cf41a78f919b20df1d8e7fd644
afpro <admin@afpro.net>
2018-09-01 00:24:09 +0800

    with line marker

---
+++ src/main/kotlin/net/afpro/idea/lancet/LancetKotlinRunLineMarkerContributor.kt
@@ -0,0 +1,11 @@
+package net.afpro.idea.lancet
+
+import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.psi.PsiElement
+
+
+class LancetKotlinRunLineMarkerContributor : RunLineMarkerContributor() {
+    override fun getInfo(element: PsiElement): Info? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
