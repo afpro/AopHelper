4e5dddc683a421a2c7b6fb7e4a24b9adf969b326
afpro <admin@afpro.net>
2018-09-01 02:48:27 +0800

    lancet java target marker

--- src/main/kotlin/net/afpro/idea/aophelper/base/AopSourceLineMarkerProvider.kt
+++ src/main/kotlin/net/afpro/idea/aophelper/base/AopSourceLineMarkerProvider.kt
@@ -1,8 +1,10 @@
 package net.afpro.idea.aophelper.base
 
+import com.intellij.codeHighlighting.Pass
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
+import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
 import com.intellij.util.ui.ColorIcon
 import java.awt.Color
@@ -19,22 +21,33 @@ abstract class AopSourceLineMarkerProvider : LineMarkerProvider {
 
     final override fun collectSlowLineMarkers(elements: MutableList<PsiElement>, result: MutableCollection<LineMarkerInfo<PsiElement>>) {
         elements.asSequence()
-                .filter { aopType(it, true) != ElementAopType.NOBODY }
+                .filter { aopType(it, true) == ElementAopType.SLOW_SOURCE }
                 .mapNotNull(this::createLineMarker)
                 .forEach { result.add(it) }
     }
 
-    private fun createLineMarker(element: PsiElement) : LineMarkerInfo<PsiElement>? {
+    private fun createLineMarker(element: PsiElement): LineMarkerInfo<PsiElement>? {
+        val anchor = aopAnchor(element)
         val targets = aopTargets(element).toList()
-        if (targets.isEmpty())
-            return null
+        val icon = createIcon(targets.isNotEmpty())
 
-        return NavigationGutterIconBuilder.create(createIcon())
-                .setTargets(targets)
-                .createLineMarkerInfo(aopAnchor(element))
+        return if (targets.isEmpty()) {
+            LineMarkerInfo(
+                    anchor,
+                    anchor.textRange,
+                    icon,
+                    Pass.LINE_MARKERS,
+                    null,
+                    null,
+                    GutterIconRenderer.Alignment.LEFT)
+        } else {
+            NavigationGutterIconBuilder.create(icon)
+                    .setTargets(targets)
+                    .createLineMarkerInfo(anchor)
+        }
     }
 
-    protected open fun createIcon(): Icon {
+    protected open fun createIcon(hasNav: Boolean): Icon {
         return ColorIcon(12, Color(0x33, 0x66, 0x99))
     }
 
